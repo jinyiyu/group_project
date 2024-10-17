d2fd559f62813558731585970d7514c3f94611df
Jinyi Yu <yujinyiicxk@gmail.com>
2024-10-17 16:21:09 -0500

    added report schema and seeds

--- server/seeds.js
+++ server/seeds.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 const User = require("./models/userSchema");
+const Document = require("./models/documentSchema");
+const Report = require("./models/reportSchema");
+
 const seedUsers = require("./data/userData");
 const seedDocument = require("./data/documentData");
-const Document = require("./models/documentSchema");
+const seedReports = require("./data/reportData");
 
 const start = async () => {
   try {
@@ -17,14 +20,18 @@ const start = async () => {
 
 const seedDatabase = async () => {
   try {
-    await User.deleteMany({});
+    // await User.deleteMany({});
     await Document.deleteMany({});
+    await Report.deleteMany({});
+
     console.log("Existing data deleted successfully.");
 
     const createdUsers = await User.insertMany(seedUsers);
     const createdDocument = await Document.insertMany(seedDocument);
+    const createdReports = await Report.insertMany(seedReports);
     console.log("Users seeded successfully");
     console.log("Document seeded successfully");
+    console.log("Reports seeded successfully.");
   } catch (err) {
     console.error("Error seeding database:", err);
   }
