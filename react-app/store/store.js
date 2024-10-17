268f9439f05cddac1573ff83fe3812ca1d01c3c3
Dailin <dailin@Dereks-Mac.local>
2024-10-16 21:44:46 -0500

    BGP-23[FRONTEND] set up redux with example(todoSlice)

---
+++ react-app/store/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+import todoReducer from './todoSlice/todo.slice';
+
+const store = configureStore({
+  reducer: {
+    todo: todoReducer,
+  },
+  // // you don't have to use third-party libraries to enable Redux devTools anymore
+  devTools: true,
+  // // the getDefaultMiddleware function contains the thunk middleware, no need to rely on third-party libraries
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+});
+
+export default store;
