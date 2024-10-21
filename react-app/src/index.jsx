dee6d0093ea0fedc1474740a00fb2093106d32b2
Jinyi Yu <yujinyiicxk@gmail.com>
2024-10-21 13:52:58 -0500

    /

--- react-app/src/index.jsx
+++ react-app/src/index.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById("root")).render(
       {/* <EmployeeSummaryView /> */}
       {/* <Housing /> */}
       {/* <UserVisaPage /> */}
-      {/* <OnBoarding></OnBoarding> */}
+      {/* <OnBoarding /> */}
     </Provider>
   </StrictMode>
 );
