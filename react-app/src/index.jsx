92ea20c108e3cb4d800417b95922c88dca3c160c
Shengyi Zhang <shen.zhang0907@gmail.com>
2024-10-21 12:45:27 -0400

    fixed CommentModal

--- react-app/src/index.jsx
+++ react-app/src/index.jsx
@@ -2,14 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./store/index.js";
-import EmployeeSummaryView from "./components/employeeSummaryView.jsx";
+import store from "./store/store.js";
+// import EmployeeSummaryView from "./components/employeeSummaryView.jsx";
+import UserVisaPage from "./pages/UserVisaPage.jsx";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
+import Housing from "./pages/Housing.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <EmployeeSummaryView />
+      {/* <EmployeeSummaryView /> */}
+      <Housing />
+      {/* <UserVisaPage /> */}
     </Provider>
   </StrictMode>
 );
