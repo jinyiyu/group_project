b01e4f3be2702f90214c260d11427a0fe1bc3231
Hieu Tran <hieunht97@gmail.com>
2024-10-24 16:32:23 -0400

    update login register

--- server/middlewares/AuthMiddleWare.js
+++ server/middlewares/AuthMiddleWare.js
@@ -43,35 +43,12 @@ const inviteUrlValidation = (req, res, next) => {
     // Attach the decoded email to the request body for further use
     req.body.email = decoded.email;
 
-    // Optionally, you can attach other decoded data like fullName if needed
-    // req.body.fullName = decoded.fullName;
-
-    next(); // Move to the next middleware or route handler
+    next();
   } catch (error) {
     console.error("Token verification failed:", error.message);
     return res.status(401).json({ message: "Invalid URL or URL expired." });
   }
 };
-
-// const inviteUrlValidation = (req, res, next) => {
-//     const token = req.params.token;
-//     // test
-//     console.log(token);
-//     req.body.email = "test@gmail.com"
-
-//     // if (!token || validator.isEmpty(token)) {
-//     //     return res.status(403).json({ message: "Invalid link." });
-//     // }
-
-//     // try {
-//     //     const decoded = jwt.verify(token, process.env.INVITE_TOKEN_SECRET);
-//     //     req.body.email = decoded.email;
-//     // } catch (error) {
-//     //     return res.status(401).json({ message: "Invalid url or url expired." });
-//     // }
-
-//     next();
-// };
 
 exports.accessValidation = accessValidation;
 exports.inviteUrlValidation = inviteUrlValidation;
